9a26f5add80bfb4fc0d1d5e6d8d5e05808d42d5c
Reem Tamir <reem.tamir36@gmail.com>
2022-07-20 00:04:33 +0300

    fix css

--- script.js
+++ script.js
@@ -33,16 +33,17 @@ function showCard() {
   grid.appendChild(elCard);
 
   elCard.classList.add(...['card', 'm-auto', 'col', 'col-4']);
-  elCard.style.height = 165 + 'px';
+  elCard.style.height = 170 + 'px';
   elCard.innerHTML = `
-      <ul class='list-group list-group-flush '>
-        <li class='list-group-item card-name w-50  text-center m-auto'>${
+      <ul class='list-group list-group-flush   '>
+        <li class='list-group-item card-name w-75  text-center m-auto d-flex flex-column'>${
           nameValue[0].toUpperCase() + nameValue.slice(1).toLowerCase()
         }</li>
-        <li class='list-group-item w-50 card-age text-center m-auto'>${ageValue}</li>
-        <li class='list-group-item w-50 card-country text-center m-auto'>${countryValue}</li>
-        <li class='list-group-item w-50 card-gender text-center m-auto'>${gender}</li>
-      </ul>`;
+        <li class='list-group-item w-75 card-age text-center m-auto'>${ageValue}</li>
+        <li class='list-group-item w-75 card-country text-center m-auto'>${countryValue}</li>
+        <li class='list-group-item w-75 card-gender text-center m-auto'>${gender}</li>
+      </ul>
+      `;
 
   elCard.style.backgroundColor = bgColor.value;
   nameInput.value = '';
